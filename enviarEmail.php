634d9dea6914b511d1e48cde3dce993894079176
niczc <50963474+niczc@users.noreply.github.com>
2020-11-04 21:20:19 -0300

    ATUALIZAÇÃO CORRETA PARA O ENVIO DE EMAILS

--- enviarEmail.php
+++ enviarEmail.php
@@ -1,94 +1,48 @@
 <?php
+require_once('src/PHPMailer.php');
+require_once('src/SMTP.php');
+require_once('src/Exception.php');
 
-if(isset($_POST['email'])  && !empty($_POST['email'])) {
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-    $nome = addslashes($_POST['name']);
-    $email = addslashes($_POST['name']);
-    $assunto = addslashes($_POST['subject']);
-    $menssagem = addslashes($_POST['message']);
-    $data_envio = date('d/m/Y');
-    $hora_envio = date('H:i:s');
 
+$nome = $_POST['name'] ? $_POST['name']: 'Não informado';
+$email = $_POST['email'] ? $_POST['email']: 'Não informado';
+$assunto = $_POST['subject'] ? $_POST['subject']: 'Não informado';
+$mensagem = $_POST['message'] ? $_POST['message']: 'Não informado';
+
+if ($email && $mensagem) {
+
+
+$mail = new PHPMailer();
+
+	$mail->isSMTP();
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = true;
+	$mail->Username = '@gmail.com';  //insere aqui o email da empresa
+	$mail->Password = '1234'; // a senha do email
+	$mail->Port = 587;
+
+	$mail->setFrom('@gmail.com');  //insere aqui o email novamente, pois é atraves dessa linha que ele enviará o email
+	$mail->addAddress('@gmail.com'); //aqui é onde ele recebe  o destinatario (o da propria empresa, ja que ela vai receber) e caso seja mais de um, basta acrecentar mais linhas dessa
+
+
+	//as 3 linhas abaixo é onde vc pode modificar o texto do email e onde ele receberá os parametros do formulario
+	$mail->isHTML(true);
+	$mail->Subject = 'Teste de email via gmail Serviços Crystal';
+	$mail->Body = "Nome: {$nome}<br>
+					  Email: {$email}<br>
+					  Mensagem: {$mensagem}<br>"
+					  ;
+
+	if($mail->send()) {
+		echo "<script>window.location='contato.php';alert('$nome, sua mensagem foi enviada com sucesso! Estaremos retornando em breve');</script>";
+	} else {
+		echo 'Email nao enviado';
+		}
+
+} else {
+	echo 'Email não enviado: informar o email e a mensagem';
 }
-
-// $to = "nicolas.leao98@gmail.com";
-// $subject = "Contato - Programador Br";
-// $body = "Nome: ".$nome. "\r\n".
-//         "Email: ". $email. "\r\n".
-//         "Mensagem: ". $message. "\r\n";
-
-// $header = "From:nicolas.leao99@gmail.com"."\r\n".
-//             "Reply-to:".$email."\r\n".
-//             "X=Mailer:PHP/".phpversion();
-
-
-// if(mail($to,$subject,$body,$header)) {
-//     echo("Sucess!!");
-// }else {
-//     echo("Error!!");
-// }
-
-// Compo E-mail
-$arquivo = "
-<style type='text/css'>
-body {
-margin:0px;
-font-family:Verdane;
-font-size:12px;
-color: #666666;
-}
-a{
-color: #666666;
-text-decoration: none;
-}
-a:hover {
-color: #FF0000;
-text-decoration: none;
-}
-</style>
-  <html>
-      <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
-          <tr>
-            <td>
-<tr>
-               <td width='500'>Nome:$nome</td>
-              </tr>
-              <tr>
-                <td width='320'>E-mail:<b>$email</b></td>
-   </tr>
-    <tr>
-                <td width='320'>Telefone:<b>$assunto</b></td>
-              </tr>
-   <tr>
-                <td width='320'>Mensagem:$menssagem</td>
-              </tr>
-              <tr>
-                <td width='320'>Mensagem:</td>
-              </tr>
-          </td>
-        </tr>
-        <tr>
-          <td>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></td>
-        </tr>
-      </table>
-  </html>
-";
-
-$emailenviar = "nicolas.leao99@gmail.com";
-$destino = $emailenviar;
-$assunto = "Contato pelo Site";
-
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'From: $nome <$email>';
-
-$enviaremail = mail($destino, $assunto, $arquivo, $headers);
-  if($enviaremail){
-  $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
-  echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
-  } else {
-  $mgm = "ERRO AO ENVIAR E-MAIL!";
-  echo "";
-  }
-
-?>
